7b3964cb5140e222cc8516277dc2b81a775b6663
Samer A <thedev.samer@gmail.com>
2022-07-29 11:44:32 +0300

    modified the captcha logic & added html markup with form

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
 require("dotenv").config();
-const request = require("request");
 const express = require("express");
+const fetch = require("node-fetch");
+const { stringify } = require("querystring");
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/subscribe", (req, res, next) => {
-  const { captcha } = req.body;
-  if (!captcha) {
-    res.status(400).json({ success: false, mssg: "Please select captcha." });
-  }
+app.post("/subscribe", async (req, res) => {
+  if (!req.body.captcha)
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please select captcha" });
 
+  // Secret key
   const secretKey = process.env["RECAPTCHA_SECRET_KEY"];
-  const remoteIp = req.connection.remoteAddress;
-  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${remoteIp}`;
 
-  request(verifyUrl, (err, res, body) => {
-    body = JSON.parse(body);
-
-    if (!body.success) {
-      return res
-        .status(400)
-        .json({ success: false, mssg: "CAPTCHA select failed." });
-    }
-
-    res.status(200).json({ success: true, mssg: "CAPTCHA select succeeded." });
+  // Verify URL
+  const query = stringify({
+    secret: secretKey,
+    response: req.body.captcha,
+    remoteip: req.connection.remoteAddress,
   });
+  const verifyURL = `https://google.com/recaptcha/api/siteverify?${query}`;
+
+  // Make a request to verifyURL
+  const body = await fetch(verifyURL).then((res) => res.json());
+
+  // If not successful
+  if (body.success !== undefined && !body.success)
+    return res
+      .status(400)
+      .json({ success: false, msg: "Captcha verification failed." });
+
+  // If successful
+  return res.status(200).json({ success: true, msg: "Captcha passed." });
 });
 
-const port = process.env["PORT"] || 3000;
-app.listen(port, () => {
-  console.log(`Node server is listening on port ${port}`);
-});
+app.listen(3000, () => console.log("Server started on port 3000"));
