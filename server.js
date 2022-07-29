5db13991ff5328bc5f02860dd7a244ab4a4fb65e
Samer A <thedev.samer@gmail.com>
2022-07-29 11:29:28 +0300

    wrote the captcha validation logic code

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 require("dotenv").config();
+const request = require("request");
 const express = require("express");
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/subscribe", (req, res, next) => {
+  const { captcha } = req.body;
+  if (!captcha) {
+    res.status(400).json({ success: false, mssg: "Please select captcha." });
+  }
+
+  const secretKey = process.env["RECAPTCHA_SECRET_KEY"];
+  const remoteIp = req.connection.remoteAddress;
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${remoteIp}`;
+
+  request(verifyUrl, (err, res, body) => {
+    body = JSON.parse(body);
+
+    if (!body.success) {
+      return res
+        .status(400)
+        .json({ success: false, mssg: "CAPTCHA select failed." });
+    }
+
+    res.status(200).json({ success: true, mssg: "CAPTCHA select succeeded." });
+  });
+});
 
 const port = process.env["PORT"] || 3000;
 app.listen(port, () => {
